dd099b733d2df341404d6a55ecd41a66d821f1aa
Puneet Oberoi <puneet@reyeselsamad.com>
2018-09-28 18:34:34 +0500

    Added key_dev file

---
+++ src/config/keys_dev.js
@@ -0,0 +1,9 @@
+const keys = {
+  SECRET: '7998def27f67fd6a6e5f0d528d42b962',
+  HOST: 'localhost',
+  PORT: 3000 || process.env.PORT,
+  GOOGLE_MAPS_API_KEY: 'AIzaSyALC0RdIvbBvmFCj8P5IBClasErYh0QqdQ',
+  WEBSITE_URL: 'http://localhost:4000/graphql',
+};
+
+module.exports = keys;
